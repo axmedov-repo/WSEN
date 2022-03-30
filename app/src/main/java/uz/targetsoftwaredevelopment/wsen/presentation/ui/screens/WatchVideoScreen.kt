7b366fef53022fc6f8296f1b49b2f36e33f429af
= <=>
2022-03-30 23:35:58 +0500

    create update fragments twenty nine

--- app/src/main/java/uz/targetsoftwaredevelopment/wsen/presentation/ui/screens/WatchVideoScreen.kt
+++ app/src/main/java/uz/targetsoftwaredevelopment/wsen/presentation/ui/screens/WatchVideoScreen.kt
@@ -6,6 +6,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.bumptech.glide.Glide
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import dagger.hilt.android.AndroidEntryPoint
@@ -25,21 +26,16 @@ class WatchVideoScreen : Fragment(R.layout.screen_watch_video) {
 
         loadData()
 
-        player = ExoPlayer.Builder(requireContext()).build()
-        val onlineUri: Uri = Uri.parse(args.videoData.video)
-        val mediaItem: MediaItem = MediaItem.fromUri(onlineUri)
-        watchVideoView.player = player
-        player.setMediaItem(mediaItem)
-        player.prepare()
-
-
         likeVideoImg.setOnClickListener {
             isLike = if(isLike){
                 likeVideoImg.setImageResource(R.drawable.ic_heart_unlike)
+                // TODO: view model change like
                 false
             }else{
                 likeVideoImg.setImageResource(R.drawable.ic_heart)
                 true
+                // TODO: change like
+
             }
         }
 
@@ -47,9 +43,48 @@ class WatchVideoScreen : Fragment(R.layout.screen_watch_video) {
     }
 
     private fun loadData() {
-//        binding.apply {
-//            if(args.videoData.)
-//        }
+        binding.apply {
+
+            player = ExoPlayer.Builder(requireContext()).build()
+            val onlineUri: Uri = Uri.parse(args.videoData.video)
+            val mediaItem: MediaItem = MediaItem.fromUri(onlineUri)
+            watchVideoView.player = player
+            player.setMediaItem(mediaItem)
+            player.prepare()
+
+            if(args.videoData.is_liked_by_currentUser){
+                likeVideoImg.setImageResource(R.drawable.ic_heart)
+            }else{
+                likeVideoImg.setImageResource(R.drawable.ic_heart_unlike)
+            }
+
+            if(args.videoData.status=="1"){
+                statusColor.setBackgroundResource(R.drawable.shape_status_waiting)
+                statusTextTv.text = getString(R.string.waiting)
+            }else{
+                statusColor.setBackgroundResource(R.drawable.shape_status_finished)
+                statusTextTv.text = getString(R.string.finished)
+            }
+
+            Glide.with(accountImgView.context)
+                .load(args.videoData.owner?.photo)
+                .placeholder(R.drawable.default_profile_img2)
+                .error(R.drawable.default_profile_img2)
+                .into(accountImgView)
+
+            accountNameTv1.text = args.videoData.owner?.username
+
+            likesCountTv.text = args.videoData.like.toString()
+
+            val year = args.videoData.created_at?.substring(0,3)
+            dateYearTv.text = year
+
+            val monthData = args.videoData.created_at?.substring(5,9)
+            dataMonthTv.text = monthData
+
+            watchDescriptionTv.text = args.videoData.desc
+
+        }
     }
 
     override fun onStop() {
